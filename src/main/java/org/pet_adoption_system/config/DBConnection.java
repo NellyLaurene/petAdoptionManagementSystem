e6399540c76de1fb0596f189ef2fabbd613d77a0
NellyLaurene <116671674+NellyLaurene@users.noreply.github.com>
2025-05-28 06:07:06 +0300

    Update DBConnection.java

--- src/main/java/org/pet_adoption_system/config/DBConnection.java
+++ src/main/java/org/pet_adoption_system/config/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static String db_url = "jdbc:postgresql://localhost:5432/pet_adoption_system_db";
     private static String db_user = "postgres";
-    private static String db_password = "27022021";
+    private static String db_password = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(db_url, db_user, db_password);
